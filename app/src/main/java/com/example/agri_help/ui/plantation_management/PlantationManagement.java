cc652e37bf9259e9329ae827fdb18807bfd97d48
rauf-pk <rauf.bhatti.github@gmail.com>
2023-03-23 01:43:03 +0500

    Improve and add UI skeletons

---
+++ app/src/main/java/com/example/agri_help/ui/plantation_management/PlantationManagement.java
@@ -0,0 +1,16 @@
+package com.example.agri_help.ui.plantation_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agri_help.R;
+
+public class PlantationManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plantation_management);
+    }
+}
