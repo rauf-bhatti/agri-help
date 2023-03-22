df74172fc2ce601306103f8835008e258a0e2290
rauf-pk <rauf.bhatti.github@gmail.com>
2023-03-23 01:43:03 +0500

    Improve and add UI skeletons

--- app/src/main/java/com/example/agri_help/MainActivity.java
+++ app/src/main/java/com/example/agri_help/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.agri_help;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_plantation);
+        Intent intent = new Intent(this, MonitorPlantation.class);
+        startActivity(intent);
     }
 }
